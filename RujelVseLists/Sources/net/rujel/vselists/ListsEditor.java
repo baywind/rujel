3747e7d5f80a90d51d73434135acc6163fb8dab1
gena <genkush@rujel.net>
2010-01-11 14:52:28 +0300

    import functionality postponed

--- RujelVseLists/Sources/net/rujel/vselists/ListsEditor.java
+++ RujelVseLists/Sources/net/rujel/vselists/ListsEditor.java
@@ -29,6 +29,7 @@
 
 package net.rujel.vselists;
 
+/*
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -36,6 +37,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CodingErrorAction;
+*/
 import java.util.Enumeration;
 import java.util.logging.Logger;
 
@@ -498,6 +500,7 @@ public class ListsEditor extends com.webobjects.appserver.WOComponent {
 		return (Number)item.valueForKey((showAll)?"allCount":"currCount");
 	}
 	
+	/*
 	public InputStream uploadStream;
 	public NSMutableDictionary uploadDict = new NSMutableDictionary(); 
 		
@@ -563,4 +566,6 @@ public class ListsEditor extends com.webobjects.appserver.WOComponent {
 		uploadDict.removeAllObjects();
 		return response;
 	}
+	*/
+	
 }
